1eb03924d99db8f9ed2e61c34af3f34f7e0f66e6
OmHiwase <hiwaseom9@gmail.com>
2025-11-04 17:31:07 +0530

    changed q1

--- Task 2/Om/om sum of digit till single digit.c
+++ Task 2/Om/om sum of digit till single digit.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int a,b,s=0;
+int a,b,s;
 printf("enter a number");
 scanf("%d",&a);
 
-while (a!=0)
+while (a>9)
+{
+s=0;
+while(a!=0)
 {
 b=a%10;
 a=a/10;
 s=s+b;
 }
+a=s;
+}
 
-printf("the sum of digits is %d",s);
-
+printf("the sum of digits is %d\n",s);
 return 0;
 }
